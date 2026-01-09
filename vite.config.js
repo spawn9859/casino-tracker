5a713c0d45c37e082923c76397a265515fe7a165
Zach Allen <spawn9859@gmail.com>
2026-01-08 18:22:46 -0600

    Initial commit2

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: './', // Relative paths for static hosting
+    base: '/casino-tracker/', // Set base to repository name for GitHub Pages
     build: {
         outDir: 'dist',
         assetsDir: 'assets',
